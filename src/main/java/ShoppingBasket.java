f25e1f3af644f3ab0326e449dd999af7ba548dba
MatthiasGu <mj.gudenas@gmail.com>
2019-06-30 23:35:45 +0100

    Adding items to basket

--- src/main/java/ShoppingBasket.java
+++ src/main/java/ShoppingBasket.java
@@ -1,19 +1,35 @@
+import model.Item;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ShoppingBasket {
 
-    private List<String> items;
+    private Map<Item, Integer> items;
 
-    public ShoppingBasket(List<String> items) {
+    public ShoppingBasket(Map<Item, Integer> items) {
         this.items = items;
     }
 
     public ShoppingBasket() {
-        this.items = new ArrayList<>();
+        this.items = new HashMap<>();
     }
 
     public boolean isEmpty() {
         return items.size() == 0;
+    }
+
+    public void addItem(Item item) {
+        if (items.containsKey(item)) {
+            items.put(item, items.get(item) + 1);
+        } else {
+            items.put(item, 1);
+        }
+    }
+
+    public Map<Item, Integer> getItems() {
+        return items;
     }
 }
--- src/test/java/ShoppingBasketShould.java
+++ src/test/java/ShoppingBasketShould.java
@@ -1,4 +1,6 @@
 
+import model.Item;
+import model.Soup;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,6 +10,16 @@ public class ShoppingBasketShould {
     public void beEmptyByDefault() {
         ShoppingBasket basket = new ShoppingBasket();
         Assert.assertTrue(basket.isEmpty());
+    }
+
+    @Test
+    public void beAbleToAddItems() {
+        ShoppingBasket basket = new ShoppingBasket();
+        Item soup = new Soup();
+        basket.addItem(soup);
+        Assert.assertEquals(1, basket.getItems().size());
+        Assert.assertTrue(basket.getItems().keySet().contains(soup));
+        Assert.assertEquals(1, (int) basket.getItems().get(soup));
     }
 }
 
